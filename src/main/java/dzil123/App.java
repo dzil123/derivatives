5d440c668ecf7f8867d98c13fa4624ef4bc8537b
Daniel Z <5725958+dzil123@users.noreply.github.com>
2018-10-23 11:37:44 -0700

    Cleanup App.java, new example

--- src/main/java/dzil123/App.java
+++ src/main/java/dzil123/App.java
@@ -10,43 +10,21 @@ public class App {
         System.out.println();
     }
 
-
     public static void main(String[] args) {
-        //derive(Product.chain(new Derivable[]{new Product(new X(), new X()), new X(), new Constant(2)}));
-
-        // Constant o = new Constant(1);
-        // Constant t = new Constant(2);
-        // Constant th = new Constant(3);
-        // X x = new X();
-
-        // Monomial a = new Monomial(new Variable[]{t,x,x,t,th,x});
-        // System.out.println(a);
-        // System.out.println(a.simplify());
-        // Derivable b = a.simplify().derive();
-        // System.out.println(b);
-        // Monomial c = new Monomial(b);
-        // System.out.println(c.simplify());
-
-        Constant a = new Constant(6);
-        Constant b = new Constant(-9);
-        Constant c = new Constant(4);
-        Constant t = new Constant(3);
+        Constant a = new Constant(3);
+        Constant b = new Constant(2);
+        Constant c = new Constant(1);
+        Constant d = new Constant(1);
         X x = new X();
 
-        Exponent term2 = new Exponent(x, t);
-        Product term = new Product(a, term2);
-        Monomial first = new Monomial(term);
-        //System.out.println(first);
-        //System.out.println(first.simplify());
+        Exponent term1 = new Exponent(x, a);
+        Exponent term2 = new Exponent(x, b);
+        Exponent term3 = new Exponent(x, c);
+        Constant term4 = d;
 
-        Monomial second = new Monomial(new Product(b, x));
-        Monomial third = new Monomial(c);
+        Derivable equation = Sum.chain(new Derivable[]{term1, term2, term3, term4});
 
-        Derivable answer = Sum.chain(new Derivable[]{first, second, third});
-
-        System.out.println(answer);
-        //System.out.println(answer.derive());
-        System.out.println(answer.derive().simplify());
+        derive(equation);
 
     }
 }
