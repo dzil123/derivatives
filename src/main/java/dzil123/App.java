c097db42d5345e256d0b7ce0ca406e90092f0c98
Daniel Z <5725958+dzil123@users.noreply.github.com>
2018-10-25 07:54:51 -0700

    Reverse Polish Notation Parser
    + App example

--- src/main/java/dzil123/App.java
+++ src/main/java/dzil123/App.java
@@ -1,5 +1,8 @@
 package dzil123;
 
+import dzil123.input.*;
+import java.util.Scanner;
+
 public class App {
 
     public static void derive(Derivable derive) {
@@ -11,20 +14,30 @@ public class App {
     }
 
     public static void main(String[] args) {
-        Constant a = new Constant(3);
-        Constant b = new Constant(2);
-        Constant c = new Constant(1);
-        Constant d = new Constant(1);
-        X x = new X();
 
-        Exponent term1 = new Exponent(x, a);
-        Exponent term2 = new Exponent(x, b);
-        Exponent term3 = new Exponent(x, c);
-        Constant term4 = d;
+        Polish parser = new Polish();
+        //Scanner input = new Scanner(System.in);
+        //Scanner input = new Scanner("x 1 ^ 4 * 5 x * + ; ");
+        Scanner input = new Scanner("1 1 + 2 + 3 1 1 + 7 - 15 / * - ;");
 
-        Derivable equation = Sum.chain(new Derivable[]{term1, term2, term3, term4});
+        parser.parseTokenLoop(input);
 
-        derive(equation);
+        derive(parser.getResult());
+        
+        // Constant a = new Constant(3);
+        // Constant b = new Constant(2);
+        // Constant c = new Constant(1);
+        // Constant d = new Constant(1);
+        // X x = new X();
+
+        // Exponent term1 = new Exponent(x, a);
+        // Exponent term2 = new Exponent(x, b);
+        // Exponent term3 = new Exponent(x, c);
+        // Constant term4 = d;
+
+        // Derivable equation = Sum.chain(new Derivable[]{term1, term2, term3, term4});
+
+        // derive(equation);
 
     }
 }
---
+++ src/main/java/dzil123/input/Polish.java
@@ -0,0 +1,79 @@
+package dzil123.input;
+
+import java.util.*;
+
+import dzil123.*;
+
+public class Polish {
+    private Stack<Derivable> tokens;
+
+    public Polish() {
+        this.tokens = new Stack<Derivable>();
+    }
+
+    public Derivable getResult() {
+        return tokens.peek();
+    }
+
+    public void parseTokenLoop(Scanner input) {
+        while (true) {
+            //System.out.print("Enter> ");
+            String token = input.next();
+            if (token.charAt(0) == ';') {
+                break;
+            }
+
+            this.parseToken(token);
+        }
+    }
+
+    public void parseToken(String token) {
+        Derivable token1;
+        Derivable token2;
+
+        try {
+            double value = Double.parseDouble(token);
+            this.addConstantToken(value);
+        } catch (NumberFormatException e) {
+            switch (token.charAt(0)) {
+                case 'x':
+                    this.addToken(new X());
+                    break;
+                case '*':
+                    token2 = this.getToken();
+                    token1 = this.getToken();
+                    this.addToken(new Product(token1, token2));
+                    break;
+                case '+':
+                    token2 = this.getToken();
+                    token1 = this.getToken();
+                    this.addToken(new Sum(token1, token2));
+                    break;
+                case '^':
+                    token2 = this.getToken();
+                    token1 = this.getToken();
+                    this.addToken(new Exponent(token1, (Constant)token2));
+                    break;
+                case ' ':
+                    break;
+                default:
+                    throw new RuntimeException();
+            }
+        }
+    }
+
+    public void addConstantToken(double value) {
+        Constant token = new Constant(value);
+        this.tokens.push(token);
+    }
+
+    public void addToken(Derivable token) {
+        this.tokens.push(token);
+    }
+
+    public Derivable getToken() {
+        return this.tokens.pop();
+        //this.tokens.
+    }
+
+}
