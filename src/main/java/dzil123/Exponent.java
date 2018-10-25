91904b2200e79641621315eb604772e1552d1010
Daniel Z <5725958+dzil123@users.noreply.github.com>
2018-10-25 16:35:56 -0700

    Allow exponent to be Derivable
    (a^u)` not implemented, throws error

--- src/main/java/dzil123/Exponent.java
+++ src/main/java/dzil123/Exponent.java
@@ -3,14 +3,14 @@ package dzil123;
 import java.util.*;
 
 public class Exponent extends Derivable {
-    Constant exponent;
+    Derivable exponent;
     Derivable base;
 
     protected Exponent() {
 
     }
 
-    public Exponent(Derivable base, Constant exponent) {
+    public Exponent(Derivable base, Derivable exponent) {
         this.base = base;
         this.exponent = exponent;
     }
@@ -28,18 +28,31 @@ public class Exponent extends Derivable {
     }
 
     public Derivable derive() {
-        // Derivable[] terms = new Derivable[(int)this.exponent.value];
-        // Arrays.fill(terms, this.base);
-
-        // return Product.chain(terms).derive().simplify();
-        return this.simplify().derive();
+        Derivable simplification = this.simplify();
+        if (simplification instanceof Exponent) {
+            throw new RuntimeException(); // TODO a^u
+        }
+        
+        return simplification.derive();
     }
 
     public Derivable simplify() {
-        Derivable[] terms = new Derivable[(int)this.exponent.value];
-        Arrays.fill(terms, this.base.simplify());
-
-        return Product.chain(terms).simplify();
+        Derivable simplification = null;
+        Derivable exponent = this.exponent.simplify();
+        Derivable base = this.base.simplify();
+        
+        if (exponent.isZero()) {
+            simplification = new Constant(1);
+        } else if (exponent instanceof Constant) {
+            Constant constExponent = (Constant)exponent;
+            Derivable[] terms = new Derivable[(int)(constExponent.value)];
+            Arrays.fill(terms, this.base.simplify());
+            simplification = Product.chain(terms);
+        } else if (base instanceof Constant) {
+            return this;
+        }
+        
+        return simplification.simplify();
     }
 
     public List<Variable> deChain() {
